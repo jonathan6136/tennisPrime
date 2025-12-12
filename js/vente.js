08100192cf4a78090726c65fb26705865d79a2b3
Rohan <baviere.ruddy62@gmail.com>
2025-12-12 10:47:20 +0100

    :sparkles: feat: added events and features

--- js/vente.js
+++ js/vente.js
@@ -12,3 +12,29 @@ toggle.onclick = function(){
     toggle.classList.toggle("active");
     navigation.classList.toggle("active");
 }
+
+const itemBesoins = document.querySelectorAll(".itemBesoin");
+
+for(const itemBesoin of itemBesoins){
+    itemBesoin.addEventListener("mouseover", () => {
+        itemBesoin.style.backgroundColor = "#000";
+        itemBesoin.style.color = "#fff";    
+    })
+    itemBesoin.addEventListener("mouseout", () =>{ 
+        itemBesoin.style.backgroundColor = "#fff";
+        itemBesoin.style.color = "#000";
+    })
+}
+
+const liens = document.querySelectorAll("a");
+
+for(const lien of liens){
+    lien.addEventListener("mouseover", () =>{
+        lien.style.transition = "0.5s"
+        lien.style.color = "#D7FF55";
+    })
+    lien.addEventListener("mouseout", () =>{
+        lien.style.transition = "0.5s";
+        lien.style.color = "#fff";
+    })
+}
