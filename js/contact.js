cc1b0db4e9bc59b345ac093eced0b92b108e4806
Behsse <behsse.pro@gmail.com>
2025-12-12 11:27:47 +0100

    :sparkles: feat: Add fonction in contact.js to get formulaire data with localstorage and display this data into result.html pages

--- js/contact.js
+++ js/contact.js
@@ -1,29 +1,10 @@
+const form = document.querySelector("#formulaire");
 
-const baliseNom = document.querySelector("#nom");
-const nom = baliseNom.value;
-console.log(nom);
-const balisePrenom = document.querySelector("#prenom");
-const prenom = balisePrenom.value;
-console.log(prenom);
-const baliseEmail = document.querySelector("#email");
-const email = baliseEmail.value;
-console.log(email);
-const baliseNumero = document.querySelector("#number");
-const numero = baliseNumero.value;
-console.log(numero);
-const baliseMotif = document.querySelector("#message");
-const motif = baliseMotif.value;
-console.log(motif);
-let form = document.querySelector("#formulaire");
 form.addEventListener("submit", (e) => {
-    e.preventDefault();
-    console.log("formulaire reçu");
+    e.preventDefault()
+    const formData = new FormData(form);
+    const monObjetContact = Object.fromEntries(formData);
+    const objetEnString = JSON.stringify(monObjetContact);
+    localStorage.setItem("form", objetEnString);
+    document.location.href="result.html"
 });
-const result = {
-    nom : nom,
-    prenom: prenom,
-    adresseMail : email,
-    numeroDeTelephone : numero,
-    motif : motif,
-}
-let baliseNomFormulaire = localStorage.setItem("#nom", result.nom);
