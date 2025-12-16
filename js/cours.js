76f4bc03e2455f69094b128e04957d8782045463
jonathan <jonathan.thery6136@gmail.com>
2025-12-16 14:30:24 +0100

    sparkles: feat: page de cours js

---
+++ js/cours.js
@@ -0,0 +1,26 @@
+const links = document.querySelectorAll('.navbarContainer nav a');
+
+links.forEach(link => {
+    link.addEventListener('click', function(e) {
+        e.preventDefault();
+
+        links.forEach(l => l.classList.remove('active'));
+        this.classList.add('active');
+    });
+});
+
+
+const toggle = document.getElementById("toggle");
+const navigation = document.getElementById("navigation");
+
+document.onclick = function(element) {
+    if(element.target.id !== "toggle" && element.target.id !== "navigation"){
+        toggle.classList.remove("active");
+        navigation.classList.remove("active");
+    }
+}
+
+toggle.onclick = function(){
+    toggle.classList.toggle("active");
+    navigation.classList.toggle("active");
+}
