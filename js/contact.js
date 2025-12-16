48bef9d2c58f06ac3d8e41d33ad842fe0e17a09b
jonathan <jonathan.thery6136@gmail.com>
2025-12-16 09:27:31 +0100

    :sparkles: feat: page formulaire avec couleur sur les champs

--- js/contact.js
+++ js/contact.js
@@ -8,3 +8,75 @@ form.addEventListener("submit", (e) => {
     localStorage.setItem("form", objetEnString);
     document.location.href="result.html"
 });
+const toggle = document.getElementById("toggle");
+const navigation = document.getElementById("navigation");
+
+toggle.addEventListener("click", () => {
+  toggle.classList.toggle("active");
+  navigation.classList.toggle("active");
+});
+
+const divs = document.querySelectorAll("#nom");
+for( const div of divs){
+    div.addEventListener("mouseover", () => {
+        div.style.backgroundColor = "#00ffff";
+        console.log("souris desssus");
+        div.style.transition = "1s"
+    })
+    div.addEventListener("mouseout", () => {
+        div.style.backgroundColor = "#000";
+        console.log("souris pas desssus")
+    })
+}
+
+const divs2 = document.querySelectorAll("#prenom");
+for( const div of divs2){
+    div.addEventListener("mouseover", () => {
+        div.style.backgroundColor = "#00ff4cff";
+        console.log("souris desssus");
+        div.style.transition = "1s"
+    })
+    div.addEventListener("mouseout", () => {
+        div.style.backgroundColor = "#000";
+        console.log("souris pas desssus")
+    })
+}
+
+const divs3 = document.querySelectorAll("#email");
+for( const div of divs3){
+    div.addEventListener("mouseover", () => {
+        div.style.backgroundColor = "#ff0000ff";
+        console.log("souris desssus");
+        div.style.transition = "1s"
+    })
+    div.addEventListener("mouseout", () => {
+        div.style.backgroundColor = "#000";
+        console.log("souris pas desssus")
+    })
+}
+
+const divs4 = document.querySelectorAll("#number");
+for( const div of divs4){
+    div.addEventListener("mouseover", () => {
+        div.style.backgroundColor = "#ff008cff";
+        console.log("souris desssus");
+        div.style.transition = "1s"
+    })
+    div.addEventListener("mouseout", () => {
+        div.style.backgroundColor = "#000";
+        console.log("souris pas desssus")
+    })
+}
+
+const divs5 = document.querySelectorAll("#message");
+for( const div of divs5){
+    div.addEventListener("mouseover", () => {
+        div.style.backgroundColor = "#d0ff00ff";
+        console.log("souris desssus");
+        div.style.transition = "1s"
+    })
+    div.addEventListener("mouseout", () => {
+        div.style.backgroundColor = "#000";
+        console.log("souris pas desssus")
+    })
+}
